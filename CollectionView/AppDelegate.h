3e5e2b0cc3a3f91342a8a38295a754d4d0a1875d
Mr.jiang <414816566@qq.com>
2017-05-11 10:38:12 +0800

    Initial Commit

---
+++ CollectionView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CollectionView
+//
+//  Created by aimoke on 2017/5/11.
+//  Copyright © 2017年 zhuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CollectionView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CollectionView
+//
+//  Created by aimoke on 2017/5/11.
+//  Copyright © 2017年 zhuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
